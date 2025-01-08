4ad17cb302835fd7a615e75183336de49bcd28fd
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2025-01-08 20:11:58 +0530

    swap pro.

---
+++ swap.cpp
@@ -0,0 +1,34 @@
+//Swap Two Numbers
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, temp;
+
+    // Input first number
+    cout << "Enter Number 1: ";
+    cin >> num1;
+
+    // Input second number
+    cout << "Enter Number 2: ";
+    cin >> num2;
+
+    // Display the numbers before swapping
+    cout << "Before Swap:" << endl;
+    cout << "Number 1: " << num1 << endl;
+    cout << "Number 2: " << num2 << endl;
+
+    // Swap the numbers
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    // Display the numbers after swapping
+    cout << "After Swap:" << endl;
+    cout << "Number 1: " << num1 << endl;
+    cout << "Number 2: " << num2 << endl;
+
+    return 0;
+}
